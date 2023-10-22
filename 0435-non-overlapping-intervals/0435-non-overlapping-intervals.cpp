709708b220bc3362976dc2d907e2199c863d319c
Ishani Parashar <ish.parashar@gmail.com>
2023-10-22 23:05:05 +0530

    Time: 2222 ms (9.14%), Space: 506.4 MB (5.23%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -2,13 +2,7 @@ typedef int ll;
 #define pb push_back
 
 bool cmp(vector<ll> a, vector<ll> b) {
-    if(a[1] < b[1]) {
-        return true;
-    }
-    if(a[0]==b[0] && b[0] > b[1]) {
-        return true;
-    }
-    return false;
+    return a[1] < b[1];
 }
 
 class Solution {
@@ -16,20 +10,15 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         ll n = intervals.size();
         sort(intervals.begin(), intervals.end(), cmp);
-        for(ll i=0;i<n;i++) {
-            ll x = intervals[i][0];
-            ll y = intervals[i][1];
-            cout<<x<<" "<<y<<endl;
-        }
         ll c = 0;
-        ll upper;
-        for(ll i=0;i<n;) {
-            upper = intervals[i][1];
-            while(i<n-1 and intervals[i+1][0] < upper) {
+        ll t = intervals[0][1];
+        for(ll i=1;i<n;i++) {
+            // cout<<intervals[i][0]<<" "<<intervals[i][1]<<" "<<t<<endl;
+            if(t>intervals[i][0]) {
                 c++;
-                i++;
+            } else {
+                t = intervals[i][1];
             }
-            i++;
         }
         return c;
     }
