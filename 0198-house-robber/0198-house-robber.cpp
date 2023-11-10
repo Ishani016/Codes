6dd04f440decfdf444209db57866e38d185adf87
Ishani Parashar <ish.parashar@gmail.com>
2023-11-11 00:04:48 +0530

    Time: 0 ms (100%), Space: 8 MB (63.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,18 @@
 typedef long long int ll;
+#define pb push_back
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
         ll n = nums.size();
-        if(n==1) {
-            return nums[0];
-        }
-        if(n==2) {
-            return max(nums[0], nums[1]);
-        }
+        if(n==1) return nums[0];
         ll dp[n];
+        memset(dp, 0, sizeof(dp));
         dp[0] = nums[0];
-        dp[1] = max(nums[1], nums[0]);
+        dp[1] = max(nums[0], nums[1]);
         for(ll i=2;i<n;i++) {
-            dp[i] = max(dp[i-2]+nums[i], dp[i-1]);
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
         }
-        // for(ll i=0;i<=n-1;i++) {
-        //     cout<<dp[i]<<" ";
-        // }
         return dp[n-1];
     }
 };
